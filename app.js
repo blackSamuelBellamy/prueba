91caeea50bb87ad2c9803226a83d3b52f7dd0dad
Alejandro <alejandro.quinones@outlook.es>
2022-06-24 22:21:38 -0400

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+
+/* setInterval(() => {
+    let vh = (window.innerHeight * .01) + 'px';
+    container.style.setProperty('--var', vh);
+    console.log(vh);
+}, 200) */
+ 
